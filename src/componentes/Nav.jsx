dd568f640f7c0996f2c0a17da7a058692a7da4fe
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2025-03-06 17:54:47 -0600

    integrando bootstrap

---
+++ src/componentes/Nav.jsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <Link to={"/"} className="navbar-brand">
+            <img src="logo.jpg" alt="" width={100} />
+          </Link>
+
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to={"/about"} className="nav-link active" aria-current="page">
+                Nosotros
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    /*    <nav>
+      <Link to="/">Inicio</Link>
+      <Link to="/about">Sobre Nosotros</Link>
+    </nav> */
+  );
+}
+
+export default Navbar;
